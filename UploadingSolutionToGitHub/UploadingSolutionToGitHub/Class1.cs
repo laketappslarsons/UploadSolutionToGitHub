487c5777d9339728eacefcc1a0d668601b9b443d
laketappslarsons <laketappslarsons@yahoo.com>
2016-08-30 13:27:52 -0700

    test upload

--- UploadingSolutionToGitHub/UploadingSolutionToGitHub/Class1.cs
+++ UploadingSolutionToGitHub/UploadingSolutionToGitHub/Class1.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SPEventHandlerOverride
+namespace UploadSolution
 {
-    public class Class1
+    public class Class3
     {
+        //comment her
+    }
+
+    public class Clas4
+    {
+        //comment her
     }
 }
